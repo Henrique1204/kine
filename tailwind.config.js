f89fd8762158a128f32434e47a41c910c5ceb450
Paulo <pauloh16.pdr@gmail.com>
2024-04-16 20:31:53 -0300

    feat: Adjust styles and create Header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			boxShadow: {
+				sm: '0 1px 16px rgb(0 0 0 / 0.05);',
+			},
+			colors: {
+				'gray-400': '#848280',
+				'gray-600': '#343434',
+				'gray-700': '#1A1A1A',
+
+				'orange-500': '#FFAA55',
+				'orange-600': '#EE8822',
+				'orange-700': '#DD6600',
+
+				'white-100': '#FAFAFA',
+				'white-200': '#F2F2F2',
+			},
+		},
+	},
+	plugins: [],
 };
