47c937f7d7ffcbb4e2a3db2486c845108d55f6b3
Paulo <pauloh16.pdr@gmail.com>
2024-04-16 19:52:48 -0300

    chore: Add shirts data mock

---
+++ src/data/shirts.ts
@@ -0,0 +1,56 @@
+const shirts = [
+	{
+		id: 1,
+		name: 'Classic Tee',
+		colors: ['#FFFFFF', '#000000', '#808080'],
+		sizes: ['P', 'G'],
+		price: 29.99,
+		image:
+			'https://images.unsplash.com/photo-1618517351616-38fb9c5210c6?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	},
+	{
+		id: 2,
+		name: 'Vintage Stripes',
+		colors: ['#0000FF', '#FF0000'],
+		sizes: ['P', 'M'],
+		price: 34.99,
+		image:
+			'https://images.unsplash.com/photo-1618517351616-38fb9c5210c6?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	},
+	{
+		id: 3,
+		name: 'Summer Breeze',
+		colors: ['#FFFF00', '#008000'],
+		sizes: ['G'],
+		price: 24.99,
+		image:
+			'https://images.unsplash.com/photo-1618517351616-38fb9c5210c6?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	},
+	{
+		id: 4,
+		name: 'Urban Camouflage',
+		colors: ['#556B2F', '#708090'],
+		sizes: ['P', 'M', 'G'],
+		price: 39.99,
+		image:
+			'https://images.unsplash.com/photo-1618517351616-38fb9c5210c6?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	},
+	{
+		id: 5,
+		name: 'Sporty Look',
+		colors: ['#0000FF', '#FFFFFF'],
+		sizes: ['P', 'M'],
+		price: 29.99,
+		image:
+			'https://images.unsplash.com/photo-1618517351616-38fb9c5210c6?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	},
+	{
+		id: 6,
+		name: 'Retro Style',
+		colors: ['#FF0000', '#FFFF00'],
+		sizes: ['M', 'G'],
+		price: 27.99,
+		image:
+			'https://images.unsplash.com/photo-1618517351616-38fb9c5210c6?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	},
+];
