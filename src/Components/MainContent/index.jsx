44fd7ab97f7f907df2d920dc67a131f46e07fb26
Paulo <pauloh16.pdr@gmail.com>
2024-04-17 00:04:50 -0300

    feat: Adjust responsivity in MainContainer

--- src/Components/MainContent/index.jsx
+++ src/Components/MainContent/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export function MainContent({ children }) {
-	return <main className='p-20'>{children}</main>;
+	return <main className='p-2 py-10 md:p-10 lg:p-20'>{children}</main>;
 }
