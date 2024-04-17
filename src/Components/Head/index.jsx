c6d9b67685ba685e68669f7ef8925bfe8ed9220b
Paulo <pauloh16.pdr@gmail.com>
2024-04-16 21:50:23 -0300

    fix: Adjust Head component

---
+++ src/Components/Head/index.jsx
@@ -0,0 +1,3 @@
+export function Head({ children }) {
+	return <>{children}</>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
 'use client';
 
-import Head from 'next/head';
-
 import * as Filter from '@/Components/Filter/BasicFilter';
+import { Head } from '@/Components/Head';
 import { MainContent } from '@/Components/MainContent';
 
 export default function Home() {
@@ -16,6 +15,7 @@ export default function Home() {
 					content='Loja de Camisetas Kine, encontre a opção que melhor se molda ao seu corpo.'
 				/>
 			</Head>
+
 			<Filter.FilterField />
 		</MainContent>
 	);
